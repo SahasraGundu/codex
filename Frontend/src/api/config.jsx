3b43019a3954bdb4ee4d277a2c399ab7c4e43a84
Sahasra Gundu <sahasragundu@Sahasras-MacBook-Air.local>
2026-03-23 16:06:00 +0530

    fix token key

--- Frontend/src/api/config.jsx
+++ Frontend/src/api/config.jsx
@@ -11,7 +11,7 @@ const api = axios.create({
 // Attach token
 api.interceptors.request.use(
   config => {
-    const token = localStorage.getItem('codex_token');
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
